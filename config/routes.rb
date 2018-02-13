19a1bdacc167cf72ad462dd3f7fa9a523554cb55
Erin Land <erinland05@gmail.com>
2018-02-13 21:42:21 +0000

    Devise user works to sign in and sign up, couldn't get OmniAuth to work, commented out

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#home"
+  devise_for :users #controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  #get '/auth/github/callback' => "omniauth_callbacks#github"
 end
