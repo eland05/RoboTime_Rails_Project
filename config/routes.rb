883b4569c8b5a467413fafa637c5622952942a41
Erin Land <erinland05@gmail.com>
2018-06-22 21:51:14 +0000

    stubbed out add members page and user edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
   resources :teams
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  #get '/auth/github/callback' => "omniauth_callbacks#github"
-  get '/profile/:id', to: 'users#show', as: 'profile'
+  get '/users/auth/github/callback' => "omniauth_callbacks#github"
+  resources :users, only: [:index, :show, :edit]
 end
