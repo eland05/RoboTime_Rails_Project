02acecfc6241ebe299f34fc186781d6ba0d90f1a
Erin Land <erinland05@gmail.com>
2018-06-22 23:48:39 +0000

    cleaned up user profiles, edit and update pages, modified nav bar based on current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get '/users/auth/github/callback' => "omniauth_callbacks#github"
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 end
