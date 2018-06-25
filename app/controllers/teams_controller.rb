e2d74d43c1872f587d3396be49fcd24d929c68d8
Erin Land <erinland05@gmail.com>
2018-06-25 23:14:30 +0000

    changed strong params

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -84,6 +84,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.require(:team).permit(:name, :number, :user_id)
+      params.require(:team).permit(:name, :number, :user_id, team_members_attributes: [:_destroy, :id, :user_id, :team_id])
     end
 end
