ec3b3324094c68cb441b517ecedb1d839512475e
Erin Land <erinland05@gmail.com>
2018-06-25 22:23:20 +0000

    added user checkboxes to team form

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,5 @@ class Team < ApplicationRecord
   belongs_to :user
   has_many :team_members
   has_many :users, through: :team_members
+  accepts_nested_attributes_for :team_members, allow_destroy: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  email: "example1@gmail.com",
+  password: "12345678",
+  name: "e1"
+  )
+User.create(
+  email: "example2@gmail.com",
+  password: "12345678",
+  name: "e2"
+  )
+User.create(
+  email: "example3@gmail.com",
+  password: "12345678",
+  name: "e3"
+  )
+User.create(
+  email: "example4@gmail.com",
+  password: "12345678",
+  name: "e4"
+  )
+User.create(
+  email: "example5@gmail.com",
+  password: "12345678",
+  name: "e5"
+  )
